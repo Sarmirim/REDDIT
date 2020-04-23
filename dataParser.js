4007f079fdf89153d59ad567ff8a348662e14b21
Sarmirim <sarmirim@gmail.com>
2020-04-23 20:45:24 +0300

    removed useless console.log

--- dataParser.js
+++ dataParser.js
@@ -47,21 +47,18 @@ async function dataParser(redditLink, jsonData) {
                 }else {
                     objectData.thumbnail = children.url;
                 }
-                try {if(preview.reddit_video_preview){objectData.media = preview.reddit_video_preview.fallback_url; gif = true}}catch (e) {console.log("10")}
-
-                try {if(children.is_video == true){objectData.media = mediaFlag.reddit_video.fallback_url; gif = true}}catch (e) {console.log("11")}
-
-                try {if(mediaFlag.reddit_video){objectData.media = mediaFlag.reddit_video.fallback_url; gif = true}}catch (e) {console.log("12")}
-
-                try {if(url.match(/\b.gif+/i)) {objectData.media = children.url; gif = true}}catch (e) {console.log("13")}
+                try {if(preview.reddit_video_preview){objectData.media = preview.reddit_video_preview.fallback_url; gif = true}}catch (e) {}
+                try {if(children.is_video == true){objectData.media = mediaFlag.reddit_video.fallback_url; gif = true}}catch (e) {}
+                try {if(mediaFlag.reddit_video){objectData.media = mediaFlag.reddit_video.fallback_url; gif = true}}catch (e) {}
+                try {if(url.match(/\b.gif+/i)) {objectData.media = children.url; gif = true}}catch (e) {}
 
                 try {if(children.crosspost_parent_list[0]){
                     let parent = children.crosspost_parent_list[0];
-                    try {if(parent.reddit_video_preview){objectData.media = parent.preview.reddit_video_preview.fallback_url; gif = true}}catch (e) {console.log("10")}
-                    try {if(parent.is_video == true){objectData.media = parent.media.reddit_video.fallback_url; gif = true}}catch (e) {console.log("11")}
-                    try {if(parent.media.reddit_video){objectData.media = parent.media.reddit_video.fallback_url; gif = true}}catch (e) {console.log("12")}
-                    try {if(parent.url.match(/\b.gif+/i)) {objectData.media = parent.url; gif = true}}catch (e) {console.log("13")}
-                }}catch (e) {console.log("14")}
+                    try {if(parent.reddit_video_preview){objectData.media = parent.preview.reddit_video_preview.fallback_url; gif = true}}catch (e) {}
+                    try {if(parent.is_video == true){objectData.media = parent.media.reddit_video.fallback_url; gif = true}}catch (e) {}
+                    try {if(parent.media.reddit_video){objectData.media = parent.media.reddit_video.fallback_url; gif = true}}catch (e) {}
+                    try {if(parent.url.match(/\b.gif+/i)) {objectData.media = parent.url; gif = true}}catch (e) {}
+                }}catch (e) {}
                 // //preview.images[0].variants.gif.source.url && preview.reddit_video_preview.is_gif
             }
             objectData.gif = gif;
